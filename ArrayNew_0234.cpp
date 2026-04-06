5f1a1e6afec138760fccff4d6b1b3c2b5709c49c
Mzahdul <seeglas8@gmail.com>
2026-04-06 18:43:10 +0700

    ke2commit

--- ArrayNew_0234.cpp
+++ ArrayNew_0234.cpp
@@ -3,3 +3,12 @@ using namespace std;
 //nama Reppo : ArraydanStruct_0234
 //minimal commit : 15 menit
 
+int maint() {
+    //deklarasi dengan inisialisasi
+    int aku [5] = {10, 20 , 30, 40, 50};
+    // deklarasi array tanpa inisialisasi
+    int kamu [5];
+    // tampilan data (membaca data array)
+    cout << "data pada index 1 = " << aku[1] << endl;
+      
+}   
